080c7aa8feffbcf0be7954f7be216e5229bad385
Oscar Oceguera bibriesca <oscaroceguera@gmail.com>
2024-11-13 13:23:39 -0700

    Prebuild

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const { DOCS_URL } = process.env;
 const nextConfig: NextConfig = {
   async rewrites() {
     return [
+      // path root
       {
         source: "/:path*",
         destination: `/:path*`,
